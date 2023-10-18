f1a1beaf25d3d63e35e973ffb194618943e5da8a
Kim Iversen <kim.iversen@blinq.no>
2023-10-18 09:15:41 +0200

    Add cloud{} block for local dev. Terraform 1.6+ required

--- providers.tf
+++ providers.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = "~> 1.6"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -10,6 +12,14 @@ terraform {
       version = "~> 2.41"
     }
   }
+
+  # Included so all new workspaces can be init, validate and planned locally.
+  # Documentation: https://developer.hashicorp.com/terraform/cli/cloud/settings
+  # 
+  # Set the following environment variables before you run terraform init.
+  # - TF_CLOUD_ORGANIZATION - The name of the Terraform Cloud organization
+  # - TF_WORKSPACE - The name of the workspace in Terraform Cloud
+  cloud {}
 }
 
 provider "azurerm" {
